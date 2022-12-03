ca01fa5d05f1d7c863095050c5ca17fbcd682c24
Krzysztof Zając <krzysztof.zajac2@gmail.com>
2022-12-03 10:17:01 +0100

    Patching parse_str

--- docker_image/functions.php
+++ docker_image/functions.php
@@ -7,5 +7,15 @@ if (!function_exists("__garlic_is_array")) {
     }
 }
 
+if (!function_exists("__garlic_parse_str")) {
+    function __garlic_parse_str($str, &$out) {
+        if (str_contains($str, "GARLIC")) {
+            $out = new MagicArrayOrObject('parse_str output', false);
+        } else {
+            parse_str($str, $out);
+        }
+    }
+}
+
 
 ?>
--- docker_image/patch_plugins.sh
+++ docker_image/patch_plugins.sh
@@ -2,6 +2,8 @@
 
 if [ "$1" == '--reverse' ]; then
     find /var/www/html/ -name "*.php" -type f -exec sed -i 's/\b__garlic_is_array(/is_array(/g' {} +
+    find /var/www/html/ -name "*.php" -type f -exec sed -i 's/\b__garlic_parse_str(/parse_str(/g' {} +
 else
     find /var/www/html/ -name "*.php" -type f -exec sed -i 's/\bis_array(/__garlic_is_array(/g' {} +
+    find /var/www/html/ -name "*.php" -type f -exec sed -i 's/\bparse_str(/__garlic_parse_str(/g' {} +
 fi
