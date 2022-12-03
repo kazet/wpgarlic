39af840270a640432d5fda4c9ab3596fc4ac3e4e
Krzysztof Zając <krzysztof.zajac2@gmail.com>
2022-12-03 10:33:19 +0100

    Missing file

---
+++ nonces_storage.py
@@ -0,0 +1,32 @@
+import base64
+import os
+import re
+import typing
+
+NONCE_RE = re.compile("__GARLIC_NONCE__(.*?)__ENDGARLIC__")
+NONCE_FILENAME = os.environ.get("NONCE_FILENAME_OVERRIDE", os.path.join(os.path.dirname(__file__), 'nonces.txt'))
+
+
+def collect_nonces(plugin_name: str, outs: typing.List[dict]) -> None:
+    matches = []
+    for out in outs:
+        out_str = out.get('output', '') + out.get('stdout', '') + out.get('stderr', '')
+
+        matches.extend(re.findall(NONCE_RE, out_str))
+
+    for match in set(matches):
+        with open(NONCE_FILENAME, 'a') as f:
+            f.write("%s %s\n" % (plugin_name, base64.b64encode(match.encode("utf-8")).decode("utf-8")))
+
+
+def get_valid_nonces_for_plugin(plugin_name: str) -> typing.List[str]:
+    if not os.path.exists(NONCE_FILENAME):
+        return []
+
+    result = []
+    with open(NONCE_FILENAME, 'r') as f:
+        for line in f:
+            plugin_name_in_line, nonce_base64 = line.split()
+            if plugin_name == plugin_name_in_line:
+                result.append(base64.b64decode(nonce_base64).decode('utf-8'))
+    return result
