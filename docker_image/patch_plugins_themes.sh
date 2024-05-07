fdb20fbf5100138419212cddc44edd6b2c6cc8f5
Krzysztof Zając <krzysztof.zajac2@gmail.com>
2024-05-07 21:34:32 +0200

    Requirements bump, blocklist bump, more mocked functions

--- docker_image/patch_plugins_themes.sh
+++ docker_image/patch_plugins_themes.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 if [ "$1" == '--reverse' ]; then
+    find /var/www/html/ -name "*.php" -type f -exec sed -i 's/\b__GARLIC_PHP_SAPI\b/PHP_SAPI/g' {} +
+    find /var/www/html/ -name "*.php" -type f -exec sed -i 's/\b__garlic_php_sapi_name(/php_sapi_name(/g' {} +
     find /var/www/html/ -name "*.php" -type f -exec sed -i 's/\b__garlic_trigger_error(/trigger_error(/g' {} +
     find /var/www/html/ -name "*.php" -type f -exec sed -i 's/\b__garlic_srand(/srand(/g' {} +
     find /var/www/html/ -name "*.php" -type f -exec sed -i 's/\b__garlic_array_key_exists(/array_key_exists(/g' {} +
@@ -10,6 +12,8 @@ if [ "$1" == '--reverse' ]; then
     find /var/www/html/ -name "*.php" -type f -exec sed -i 's/\b__garlic_is_array(/is_array(/g' {} +
     find /var/www/html/ -name "*.php" -type f -exec sed -i 's/\b__garlic_parse_str(/parse_str(/g' {} +
 else
+    find /var/www/html/ -name "*.php" -type f -exec sed -i 's/\bPHP_SAPI\b/__GARLIC_PHP_SAPI/g' {} +
+    find /var/www/html/ -name "*.php" -type f -exec sed -i 's/\bphp_sapi_name(/__garlic_php_sapi_name(/g' {} +
     find /var/www/html/ -name "*.php" -type f -exec sed -i 's/\btrigger_error(/__garlic_trigger_error(/g' {} +
     find /var/www/html/ -name "*.php" -type f -exec sed -i 's/\bsrand(/__garlic_srand(/g' {} +
     find /var/www/html/ -name "*.php" -type f -exec sed -i 's/\barray_key_exists(/__garlic_array_key_exists(/g' {} +
